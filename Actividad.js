fad4d52acce3f4218f654f14925c2716662bfb7d
AndresVillani00 <andres24villani08@gmail.com>
2021-02-23 00:38:09 +0100

    Archivo PMDM

--- Actividad.js
+++ Actividad.js
@@ -1,9 +1,26 @@
-window.onload = () =>{
-    consultaAPI("Caracas", "VEN");
-    consultaAPI("Madrid", "ES");
-    consultaAPI("Roma", "ITA");
-    consultaAPI("Berlin", "GER");
-    consultaAPI("Miami", "USA");
+window.onload = () => {
+    document.getElementById("submit").addEventListener('click', tiempo);
+    
+};
+function tiempo (tem){
+    tem.preventDefault();
+    var name = document.getElementById("nombre0").value;
+    var city = document.getElementById("ciudad0").value;
+    var country = document.getElementById("pais0").value
+    if( nombre.length > 0 && pais.length > 0 && ciudad.length > 0 ) {
+    document.getElementById('ciudad1').innerHTML = ciudad0;
+    document.getElementById('pais1').innerHTML = pais0;
+    document.getElementById('nombre1').innerHTML = nombre0;
+    consultaAPI(ciudad, pais); 
+    }
+    if( nombre.length == 0 || pais.length == 0 || ciudad.length == 0 ) {
+        Swal.fire({
+          title: 'Faltan datos!',
+          text: 'Necesita agregar todos los campos',
+          icon: 'error',
+          confirmButtonText: 'X'
+        })
+    }
 }
 
 function consultaAPI(ciudad, pais) {
@@ -22,4 +39,9 @@ fetch(url)
         console.error(error);
     })
 
-
+    function verTiempo(data) {
+        var grados = Math.round(parseFloat(data.main.temp) - 273.15);
+        var maximo = Math.round(parseFloat(data.main.temp_max) - 273.15);
+        var minimo = Math.round(parseFloat(data.main.temp_min) - 273.15);
+        document.getElementById('tiempo').innerHTML = '<p> El tiempo que hace es '+grados+'. Maximo '+maximo+' Minimo '+minimo+'</p>';
+    }
